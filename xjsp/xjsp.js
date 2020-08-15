1e73b3d89aed2be13a63bbd56a55dc2f68c32957
XiaoGuYa <17870897@qq.com>
2020-08-15 15:06:11 +0800

    Update xjsp.js

--- xjsp/xjsp.js
+++ xjsp/xjsp.js
@@ -29,7 +29,7 @@ if ($request.url.indexOf("/vod/show/") != -1){
   obj.data.vodrow.islimitv3 = "0";
   obj.data.vodrow.isvip = "0";
 }
-if ($request.url.indexOf("//vod/reqplay/") != -1){
+if ($request.url.indexOf("/vod/reqplay/") != -1){
   obj.retcode = "0";
   if(obj.data.hasOwnProperty("httpurl_preview")){
   var playurl = obj.data["httpurl_preview"];
