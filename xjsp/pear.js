c124a081f6a825fcbc6371ec3c818b3e9e6908e6
XiaoGuYa <17870897@qq.com>
2020-08-17 15:21:34 +0800

    Create pear.js

---
+++ xjsp/pear.js
@@ -0,0 +1,30 @@
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/api/movie/WatchMovieNew';
+
+const checkvip = '/api/Account/CheckVip';
+
+const vipinfo = '/api/account/IndexDetail';
+
+if (url.indexOf(vip) != -1) {
+	obj["canWath"] = "true";
+  obj["hadWach"] = "true";
+	body = JSON.stringify(obj);
+ }
+
+if (url.indexOf(checkvip) != -1) {
+	obj["data"] = "1";
+   obj["value"] = "true";
+	body = JSON.stringify(obj);
+ }
+if (url.indexOf(vipinfo) != -1) {
+ 	obj["nickName"] = "Xiao";
+   obj["vipLevel"] = "5";
+   obj["vipEndTime"] = "2222-02-22";
+   obj["cartoonVip"] = "true";
+	body = JSON.stringify(obj);
+ }
+$done({body});
