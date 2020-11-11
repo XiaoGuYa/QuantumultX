4113357219f7bea1d03c7986394b51e406ef1b96
XiaoGuYa <17870897@qq.com>
2020-11-11 13:38:32 +0800

    Update pear.js

--- xjsp/pear.js
+++ xjsp/pear.js
@@ -9,6 +9,7 @@ const vipinfo = '/api/account/IndexDetail';
 const Downvip = '/api/download/CheckDownloadTorrent';
 const cartoonvip = '/api/cartoon/VipInfo';
 const cartooncheckvip = '/api/video/watch/';
+const vipinfos = '/api/movie/SeriesFilterNew/*/10/0/0/0/1';
 if (url.indexOf(vip) != -1) {
 	obj["code"] = "1";
 	obj["canWath"] = "true";
@@ -23,10 +24,10 @@ if (url.indexOf(checkvip) != -1) {
  }
 if (url.indexOf(Downvip) != -1) {
 	obj["isVip"] = "ture";
-        obj["hadLoadCount"] = "999";
-	obj["toDayHadLoadCount"] = "999";
-	obj["toDayCanLoadCount"] = "999";
-	obj["allCount"] = "999";
+        obj["hadLoadCount"] = "80";
+	obj["toDayHadLoadCount"] = "80";
+	obj["toDayCanLoadCount"] = "80";
+	obj["allCount"] = "80";
 	obj["vipLevel"] = "3";
 	body = JSON.stringify(obj);
  }
@@ -37,7 +38,13 @@ if (url.indexOf(vipinfo) != -1) {
         obj["cartoonVip"] = "true";
 	body = JSON.stringify(obj);
  }
-
+if (url.indexOf(vipinfos) != -1) {
+ 	obj.data.result.cznVipLevel = "3";
+	obj.data.result.canOnLine = "ture";
+	obj.data.result.canLoad = "ture";
+	obj.data.result.czn = "ture";
+	body = JSON.stringify(obj);
+ }
 if (url.indexOf(cartoonvip) != -1) {
  	obj["isVip"] = "ture";
         obj["vipEndTime"] = "2222-02-22";
